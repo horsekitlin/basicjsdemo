57e453ec0662179fb4d37e00488710c338d3d386
mina <mina.tsai@velocitylight.net>
2020-07-03 18:12:04 +0800

    add account screen

--- src/screens/AccountScreen/view.js
+++ src/screens/AccountScreen/view.js
@@ -1,15 +1,52 @@
-import React from "react";
-import Box from '@material-ui/core/Box';
-import { CircularProgress } from '@material-ui/core';
+import React, { useState } from "react";
+import {
+  Box,
+  TableRow,
+} from '@material-ui/core';
+import Table from '../../components/Table';
+import TableCell from '../../components/TableCell';
+import Button from '../../components/Buttons';
 
 
-const AccountScreen = ({ isLoading, ...props }) => {
-  if (isLoading) return <CircularProgress />;
+const AccountScreen = (props) => {
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+
+  const handleChangePage = () => {
+    setPage(page);
+  };
+
+  const handleChangeRowsPerPage = () => {
+    setRowsPerPage(rowsPerPage);
+  };
+
+  const headers = ['账号', '角色', '状态', '操作'];
+  const datas = [
+    { name: 'administrator', role:'admin', status: 1, opt: ''},
+    { name: 'user01', role:'manager', status: 1, opt:'edit'},
+  ];
   
   return(
-      <Box m={3} display='flex' flexDirection='column'>
-        AccountScreen 
-      </Box>
+    <Box m={3} display='flex' flexDirection='column'>
+      <Table
+        title='AccountScreen'
+        rightTitle={<Button> Export Action </Button>}
+        headers={headers}
+        children={
+          datas.map(row=> (
+            <TableRow>
+              <TableCell>{row.name}</TableCell>
+              <TableCell>{row.role}</TableCell>
+              <TableCell>{row.status}</TableCell>
+              <TableCell>{row.opt}</TableCell>
+            </TableRow>
+          ))
+        }
+        totalCount={datas.length}
+        handleChangePage={handleChangePage}
+        handleChangeRowsPerPage={handleChangeRowsPerPage}
+      />
+    </Box>
   );
 };
 
