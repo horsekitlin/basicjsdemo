7b78d7d23b0b66c677a83edb079d9718578f4f65
Tomas Lin <tomas.lin@velocitylight.net>
2020-07-08 15:39:34 +0800

    移除不需要的程式碼

--- src/components/PublicRoute.js
+++ src/components/PublicRoute.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Redirect, Route } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 
 export default ({ component: Component, ...rest }) => {
 
